5690a91751356d10702df00457d0f08cff78b1ea
HugooGFT <hugo.gaffet@gmail.com>
2024-01-29 15:47:49 +0100

    feat/Icon Tab Bar

--- src/navigation/BottomTabs.js
+++ src/navigation/BottomTabs.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import SearchScreen from '../screens/SearchScreen';
 import ProfilScreen from '../screens/ProfilScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
 const Tab = createBottomTabNavigator();
@@ -11,7 +12,29 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
   return (
   <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Search') {
+              iconName = 'search-outline';
+            } else if (route.name === 'Profil') {
+              iconName = 'person-outline';
+            }
+
+            // Renvoie l'icône
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+
+        >
           <Tab.Screen name="Home" component={HomeScreen} />
           <Tab.Screen name="Search" component={SearchScreen} />
           <Tab.Screen name="Profil" component={ProfilScreen} />
